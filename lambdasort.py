1510f5f619a3e041593409740ac529c16a5bac27
Lucas Oshiro <lucasseikioshiro@gmail.com>
2017-07-09 21:49:02 -0300

    List-based quicksort

--- lambdasort.py
+++ lambdasort.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 
-def quicksort_wrapper(A):
-    return sorted(A)
+def car(A):
+    return A[0]
 
-def quicksort(A, p, r):
-    pass
+def cdr(A):
+    return A[1:]
 
-def partition(A, p, r):
-    pass
+def insert(A, x):
+    return [x] + A
+
+def concat(A, B):
+    return A + B
+
+def quicksort(A):
+    if len(A) <= 1: return A
+    L, R = partition(A)
+    p = car(R)
+    L = quicksort(L)
+    R = quicksort(cdr(R))
+    return concat(L, concat([p], R))
+
+def partition(A):
+    p = car(A)
+    L = []
+    R = []
+
+    for x in cdr(A):
+        if x < p: L = insert(L, x)
+        else: R = insert(R, x)
+
+    R = insert(R, p)
+
+    return L, R
