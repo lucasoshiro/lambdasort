36be1fa783efa4ded39895e5a94d373bc4faee56
Lucas Oshiro <lucasseikioshiro@gmail.com>
2017-07-09 22:15:12 -0300

    Integers

--- lambdasort.py
+++ lambdasort.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+
+#boolean constants
 LAMBDA_TRUE = lambda a: lambda b: a
 LAMBDA_FALSE = lambda a: lambda b: b
 
@@ -11,12 +13,46 @@ LAMBDA_NOT = lambda a: a(LAMBDA_FALSE)(LAMBDA_TRUE)
 #if
 LAMBDA_IF = lambda c: lambda t: lambda e: c(t)(e)
 
+#integer constants
+LAMBDA_ZERO = lambda p: lambda x: x
+LAMBDA_ONE = lambda p: lambda x: p(x)
+LAMBDA_TWO = lambda p: lambda x: p(p(x))
+
+#integer operations
+LAMBDA_INCREMENT = lambda l: lambda p: lambda x: p(l(p)(x))
+LAMBDA_DECREMENT = lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda y: x)(lambda y: y)
+LAMBDA_ADD = lambda m: lambda n: n(LAMBDA_INCREMENT)(m)
+LAMBDA_SUB = lambda m: lambda n: n(LAMBDA_DECREMENT)(m)
+
+#comparators
+LAMBDA_EQZ = lambda n: n(lambda x: LAMBDA_FALSE)(LAMBDA_TRUE)
+LAMBDA_LEQ = lambda m: lambda n: LAMBDA_EQZ(LAMBDA_SUB(m)(n))
+LAMBDA_EQ = lambda m: lambda n: LAMBDA_AND(LAMBDA_LEQ(m)(n))(LAMBDA_LEQ(n)(m))
+LAMBDA_LESS = lambda m: lambda n: LAMBDA_AND(LAMBDA_LEQ(m)(n))(LAMBDA_NOT(LAMBDA_EQ(m)(n)))
+
 #boolean conversion
 def l2b(l):
     return l(True)(False)
 
 def b2l(b):
     return LAMBDA_TRUE if b else LAMBDA_FALSE
+
+#integer conversion
+def l2i(l):
+    return l(lambda x: x + 1)(0)
+
+def i2l(i):
+    l = LAMBDA_ZERO
+    for j in range(0, i):
+        l = LAMBDA_INCREMENT(l)
+
+    return l
+
+def llist2pylist(L):
+    return list(map(l2i, L))
+
+def pylist2llist(L):
+    return list(map(i2l, L))
 
 #list operators
 def car(A):
@@ -34,11 +70,16 @@ def concat(A, B):
 #quicksort
 def quicksort(A):
     if len(A) <= 1: return A
-    L, R = partition(A)
+    L, R = partition_wrapper(A)
     p = car(R)
     L = quicksort(L)
     R = quicksort(cdr(R))
     return concat(L, concat([p], R))
+
+def partition_wrapper(A):
+    B = pylist2llist(A)
+    L, R = partition(B)
+    return llist2pylist(L), llist2pylist(R)
 
 def partition(A):
     p = car(A)
@@ -46,7 +87,7 @@ def partition(A):
     L, R = [], []
 
     for x in cdr(A):
-        if x < p:
+        if l2b(LAMBDA_LESS(x)(p)):
             L, R = cons(x, L), R
         else:
             L, R = L, cons(x, R)
--- test.py
+++ test.py
@@ -5,16 +5,19 @@ from lambdasort import *
 from random import randint
 from functools import reduce
 
+l = [37, 3, 15, 17, 18, 18, 1, 33, 28, 39, 34, 37, 12, 1, 13, 30, 13, 10, 32,
+     34, 4, 34, 3, 46, 49, 8, 9, 29, 29, 30, 36, 28, 40, 38, 33, 26, 30, 2, 43,
+     46, 46, 47, 32, 26, 30, 36, 13, 44, 26, 42]
+
 class TestLambdasort(unittest.TestCase):
     def test_sorting(self):
-        l = [randint(0, 1000) for i in range(0, 1000)]
         control = sorted(l)
         l2 = quicksort(l)
         self.assertEqual(control, l2)
 
     def test_partition(self):
         lst = [3, 9, 1, 5, 0, 5, 7, 9, 0, 4]
-        left, right = partition(lst)
+        left, right = partition_wrapper(lst)
         p = car(right)
         self.assertEqual(list(filter(lambda x: x < p, left)), left)
         self.assertEqual(list(filter(lambda x: x >= p, right)), right)
@@ -45,9 +48,57 @@ class TestLambdasort(unittest.TestCase):
         self.assertFalse(l2b(LAMBDA_NOT(b2l(True))))
         self.assertTrue(l2b(LAMBDA_NOT(b2l(False))))
 
+
     def test_LAMBDA_IF(self):
         self.assertTrue(l2b(LAMBDA_IF(b2l(True))(LAMBDA_TRUE)(LAMBDA_FALSE)))
         self.assertFalse(l2b(LAMBDA_IF(b2l(False))(LAMBDA_TRUE)(LAMBDA_FALSE)))
+
+    def test_l2i(self):
+        self.assertEqual(l2i(LAMBDA_ZERO), 0)
+        self.assertEqual(l2i(LAMBDA_ONE), 1)
+        self.assertEqual(l2i(LAMBDA_TWO), 2)
+
+    def test_i2l(self):
+        n = 42
+        self.assertEqual(l2i(i2l(n)), n)
+
+    def test_LAMBDA_INCREMENT(self):
+        self.assertEqual(l2i(LAMBDA_INCREMENT(LAMBDA_TWO)), 3)
+
+    def test_LAMBDA_DECREMENT(self):
+        self.assertEqual(l2i(LAMBDA_DECREMENT(LAMBDA_TWO)), 1)
+
+    def test_LAMBDA_ADD(self):
+        self.assertEqual(l2i(LAMBDA_ADD(i2l(20))(i2l(15))), 35)
+
+    def test_LAMBDA_SUB(self):
+        self.assertEqual(l2i(LAMBDA_SUB(i2l(20))(i2l(15))), 5)
+
+    def test_LAMBDA_EQZ(self):
+        self.assertTrue(l2b(LAMBDA_EQZ(i2l(0))))
+        self.assertFalse(l2b(LAMBDA_EQZ(i2l(1))))
+
+    def test_LAMBDA_LEQ(self):
+        self.assertTrue(l2b(LAMBDA_LEQ(i2l(10))(i2l(10))))
+        self.assertTrue(l2b(LAMBDA_LEQ(i2l(10))(i2l(42))))
+        self.assertFalse(l2b(LAMBDA_LEQ(i2l(42))(i2l(10))))
+
+    def test_LAMBDA_EQ(self):
+        self.assertTrue(l2b(LAMBDA_EQ(LAMBDA_ONE)(LAMBDA_ONE)))
+        self.assertFalse(l2b(LAMBDA_EQ(LAMBDA_ONE)(LAMBDA_ZERO)))
+        self.assertFalse(l2b(LAMBDA_EQ(LAMBDA_ONE)(LAMBDA_TWO)))
+
+    def test_LAMBDA_LESS(self):
+        self.assertFalse(l2b(LAMBDA_LESS(i2l(10))(i2l(10))))
+        self.assertTrue(l2b(LAMBDA_LESS(i2l(10))(i2l(42))))
+        self.assertFalse(l2b(LAMBDA_LESS(i2l(42))(i2l(10))))
+
+    def test_llist2pylist(self):
+        self.assertEqual(llist2pylist([LAMBDA_ZERO, LAMBDA_ONE, LAMBDA_TWO]),
+                         [0, 1, 2])
+
+    def test_pylist2llist(self):
+        self.assertEqual(llist2pylist(pylist2llist([0, 1, 2])), [0, 1, 2])
 
 if __name__ == '__main__':
     unittest.main()
