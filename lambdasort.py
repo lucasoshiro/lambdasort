5e9dc9e1f9bc76d4187a6e9446b0b4eb48fb21fd
Lucas Oshiro <lucasseikioshiro@gmail.com>
2017-07-09 22:52:22 -0300

    Or, and, not

--- lambdasort.py
+++ lambdasort.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
+#boolean constants
 LAMBDA_TRUE = lambda a, b: a
 LAMBDA_FALSE = lambda a, b: b
 
+#boolean opearations
+LAMBDA_OR = lambda a, b: a(LAMBDA_TRUE, b)
+LAMBDA_AND = lambda a, b: a(b, LAMBDA_FALSE)
+LAMBDA_NOT = lambda a: a(LAMBDA_FALSE, LAMBDA_TRUE)
+
+#boolean conversion
 def l2b(l):
     return l(True, False)
 
 def b2l(b):
     return LAMBDA_TRUE if b else LAMBDA_FALSE
 
+#list operators
 def car(A):
     return A[0]
 
@@ -21,6 +29,7 @@ def insert(A, x):
 def concat(A, B):
     return A + B
 
+#quicksort
 def quicksort(A):
     if len(A) <= 1: return A
     L, R = partition(A)
