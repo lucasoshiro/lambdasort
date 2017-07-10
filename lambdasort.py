dcd376f2c6bef44898c46cc8f8832e7559f7d51c
Lucas Oshiro <lucasseikioshiro@gmail.com>
2017-07-09 21:49:02 -0300

    List-based quicksort

--- lambdasort.py
+++ lambdasort.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
-def quicksort_wrapper(A):
-    return sorted(A)
+def car(A):
+    return A[0]
 
-def quicksort(A, p, r):
-    pass
+def cdr(A):
+    return A[1:]
 
-def partition(A, p, r):
-    pass
+def cons(x, A):
+    return [x] + A
+
+def concat(A, B):
+    return A + B
+
+def quicksort(A):
+    if len(A) <= 1: return A
+    L, R = partition(A)
+    p = car(R)
+    L = quicksort(L)
+    R = quicksort(cdr(R))
+    return concat(L, concat([p], R))
+
+def partition(A):
+    p = car(A)
+
+    L, R = [], []
+
+    for x in cdr(A):
+        if x < p:
+            L, R = cons(x, L), R
+        else:
+            L, R = L, cons(x, R)
+
+    L, R = L, cons(p, R)
+
+    return L, R
--- test.py
+++ test.py
@@ -3,13 +3,24 @@
 import unittest
 from lambdasort import *
 from random import randint
+from functools import reduce
 
 class TestLambdasort(unittest.TestCase):
     def test_sorting(self):
-        l = [randint(0, 40) for i in range(0, 40)]
+        l = [randint(0, 1000) for i in range(0, 1000)]
         control = sorted(l)
-        l2 = quicksort_wrapper(l)
+        l2 = quicksort(l)
         self.assertEqual(control, l2)
+
+    def test_partition(self):
+        lst = [3, 9, 1, 5, 0, 5, 7, 9, 0, 4]
+        left, right = partition(lst)
+        p = car(right)
+        self.assertEqual(list(filter(lambda x: x < p, left)), left)
+        self.assertEqual(list(filter(lambda x: x >= p, right)), right)
+        self.assertEqual(len(left) + len(right), len(lst))
+        self.assertEqual(sorted(left + right), sorted(lst))
+        
 
 if __name__ == '__main__':
     unittest.main()
