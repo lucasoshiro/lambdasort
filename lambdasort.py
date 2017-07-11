8fd12aecf3c5d13eafc07a5a13a4801bb78bda42
Lucas Oshiro <lucasseikioshiro@gmail.com>
2017-07-11 11:46:46 -0300

    LAMBDA_INCREMENT, LAMBDA_DECREMENT, LAMBDA_ADD and LAMBDA_SUB expanded

--- lambdasort.py
+++ lambdasort.py
@@ -1,60 +1,18 @@
 #!/usr/bin/env python3
 
-
-#boolean constants
-LAMBDA_TRUE = lambda a: lambda b: a
-LAMBDA_FALSE = lambda a: lambda b: b
-
-#boolean opearations
-LAMBDA_OR = lambda a: lambda b: a((lambda a: lambda b: a))(b)
-LAMBDA_AND = lambda a: lambda b: a(b)((lambda a: lambda b: b))
-LAMBDA_NOT = lambda a: a((lambda a: lambda b: b))((lambda a: lambda b: a))
-
-#if
-LAMBDA_IF = lambda c: lambda t: lambda e: c(t)(e)
-
-#integer constants
-LAMBDA_ZERO = lambda p: lambda x: x
-LAMBDA_ONE = lambda p: lambda x: p(x)
-LAMBDA_TWO = lambda p: lambda x: p(p(x))
-
-#integer operations
-LAMBDA_INCREMENT = lambda l: lambda p: lambda x: p(l(p)(x))
-LAMBDA_DECREMENT = lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda y: x)(lambda y: y)
-LAMBDA_ADD = lambda m: lambda n: n(LAMBDA_INCREMENT)(m)
-LAMBDA_SUB = lambda m: lambda n: n(LAMBDA_DECREMENT)(m)
-
-#comparators
-LAMBDA_EQZ = lambda n: n(lambda x: (lambda a: lambda b: b))((lambda a: lambda b: a))
-LAMBDA_LEQ = lambda m: lambda n: LAMBDA_EQZ(LAMBDA_SUB(m)(n))
-LAMBDA_EQ = lambda m: lambda n: (lambda a: lambda b: a(b)((lambda a: lambda b: b)))(LAMBDA_LEQ(m)(n))(LAMBDA_LEQ(n)(m))
-LAMBDA_LESS = lambda m: lambda n: (lambda a: lambda b: a(b)((lambda a: lambda b: b)))(LAMBDA_LEQ(m)(n))((lambda a: a((lambda a: lambda b: b))((lambda a: lambda b: a)))(LAMBDA_EQ(m)(n)))
-
-#pair operations
-LAMBDA_CONS = lambda a: lambda b: lambda l: l(a)(b)
-LAMBDA_CAR = lambda p: p(lambda a: lambda b: a)
-LAMBDA_CDR = lambda p: p(lambda a: lambda b: b)
-
-#list constants
-LAMBDA_EMPTY = (lambda a: lambda b: b)
-LAMBDA_ISEMPTY = lambda l: l(lambda h: lambda t: lambda d: (lambda a: lambda b: b))((lambda a: lambda b: a))
-
-#list operations
-LAMBDA_CONCAT = (lambda r: r(r)) (lambda r: lambda l1: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(l1))(lambda l2: l2)((lambda r: lambda l2: LAMBDA_CONS(LAMBDA_CAR(l1))(r(r)(LAMBDA_CDR(l1))(l2)))(r)))
-
 #boolean conversion
 def l2b(l):
     return l(True)(False)
 
 def b2l(b):
-    return (lambda a: lambda b: a) if b else (lambda a: lambda b: b)
+    return LAMBDA_TRUE if b else LAMBDA_FALSE
 
 #integer conversion
 def l2i(l):
     return l(lambda x: x + 1)(0)
 
 def i2l(i):
-    l = (lambda p: lambda x: x)
+    l = LAMBDA_ZERO
     for j in range(0, i):
         l = LAMBDA_INCREMENT(l)
 
@@ -94,18 +52,59 @@ def quicksort_wrapper(A):
     sorted_church = quicksort(church)
     return [l2i(x) for x in ll2pl(sorted_church)]
 
-_quicksort = lambda r: lambda A: lambda LR: LAMBDA_CONCAT(r(r)(LAMBDA_CAR(LR)))(LAMBDA_CONS(LAMBDA_CAR(LAMBDA_CDR(LR)))(r(r)(LAMBDA_CDR(LAMBDA_CDR(LR)))))
-
-_quicksort2 = lambda r: lambda A: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(A))(lambda A: A)(lambda A: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(LAMBDA_CDR(A)))(A)(_quicksort(r)(A)(partition(A))))
-
-quicksort = (lambda r: r(r))(lambda r: lambda A: _quicksort2(r)(A)(A))
-
 def partition_wrapper(A):
     B = pl2ll(list(map(i2l, A)))
     LR = partition(B)
     L = LAMBDA_CAR(LR)
     R = LAMBDA_CDR(LR)
     return list(map(l2i, ll2pl(L))), list(map(l2i, ll2pl(R)))
+
+#boolean constants
+LAMBDA_TRUE = lambda a: lambda b: a
+LAMBDA_FALSE = lambda a: lambda b: b
+
+#boolean opearations
+LAMBDA_OR = lambda a: lambda b: a((lambda a: lambda b: a))(b)
+LAMBDA_AND = lambda a: lambda b: a(b)((lambda a: lambda b: b))
+LAMBDA_NOT = lambda a: a((lambda a: lambda b: b))((lambda a: lambda b: a))
+
+#if
+LAMBDA_IF = lambda c: lambda t: lambda e: c(t)(e)
+
+#integer constants
+LAMBDA_ZERO = lambda p: lambda x: x
+LAMBDA_ONE = lambda p: lambda x: p(x)
+LAMBDA_TWO = lambda p: lambda x: p(p(x))
+
+#integer operations
+LAMBDA_INCREMENT = lambda l: lambda p: lambda x: p(l(p)(x))
+LAMBDA_DECREMENT = lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda y: x)(lambda y: y)
+LAMBDA_ADD = lambda m: lambda n: n((lambda l: lambda p: lambda x: p(l(p)(x))))(m)
+LAMBDA_SUB = lambda m: lambda n: n((lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda y: x)(lambda y: y)))(m)
+
+#comparators
+LAMBDA_EQZ = lambda n: n(lambda x: (lambda a: lambda b: b))((lambda a: lambda b: a))
+LAMBDA_LEQ = lambda m: lambda n: LAMBDA_EQZ((lambda m: lambda n: n((lambda n: lambda f: lambda x: n(lambda g: lambda h: h(g(f)))(lambda y: x)(lambda y: y)))(m))(m)(n))
+LAMBDA_EQ = lambda m: lambda n: (lambda a: lambda b: a(b)((lambda a: lambda b: b)))(LAMBDA_LEQ(m)(n))(LAMBDA_LEQ(n)(m))
+LAMBDA_LESS = lambda m: lambda n: (lambda a: lambda b: a(b)((lambda a: lambda b: b)))(LAMBDA_LEQ(m)(n))((lambda a: a((lambda a: lambda b: b))((lambda a: lambda b: a)))(LAMBDA_EQ(m)(n)))
+
+#pair operations
+LAMBDA_CONS = lambda a: lambda b: lambda l: l(a)(b)
+LAMBDA_CAR = lambda p: p(lambda a: lambda b: a)
+LAMBDA_CDR = lambda p: p(lambda a: lambda b: b)
+
+#list constants
+LAMBDA_EMPTY = (lambda a: lambda b: b)
+LAMBDA_ISEMPTY = lambda l: l(lambda h: lambda t: lambda d: (lambda a: lambda b: b))((lambda a: lambda b: a))
+
+#list operations
+LAMBDA_CONCAT = (lambda r: r(r)) (lambda r: lambda l1: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(l1))(lambda l2: l2)((lambda r: lambda l2: LAMBDA_CONS(LAMBDA_CAR(l1))(r(r)(LAMBDA_CDR(l1))(l2)))(r)))
+
+_quicksort = lambda r: lambda A: lambda LR: LAMBDA_CONCAT(r(r)(LAMBDA_CAR(LR)))(LAMBDA_CONS(LAMBDA_CAR(LAMBDA_CDR(LR)))(r(r)(LAMBDA_CDR(LAMBDA_CDR(LR)))))
+
+_quicksort2 = lambda r: lambda A: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(A))(lambda A: A)(lambda A: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_ISEMPTY(LAMBDA_CDR(A)))(A)(_quicksort(r)(A)(partition(A))))
+
+quicksort = (lambda r: r(r))(lambda r: lambda A: _quicksort2(r)(A)(A))
 
 _partition2 = lambda x: lambda L: lambda R: lambda p: (lambda c: lambda t: lambda e: c(t)(e))(LAMBDA_LESS(x)(p))(LAMBDA_CONS(LAMBDA_CONS(x)(L))(R))(LAMBDA_CONS(L)(LAMBDA_CONS(x)(R)))
 
